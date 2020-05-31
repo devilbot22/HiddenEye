f375544fea6553329e7acf5c9c1a3163959a309b
AnonUD4Y <AnonUD4Y@gmail.com>
2020-05-31 13:34:36 +0530

    Update Checks.py

--- Defs/Checks.py
+++ Defs/Checks.py
@@ -203,8 +203,8 @@ def checkPermissions():
             print("{0}Permissions granted!".format(GREEN))
         else:
             print(
-                "{0}Permissions denied! Please run as '{1}sudo{0}'".format(RED, GREEN))
-            exit()
+                "{0}If possible! Please run as '{1}sudo{0}'".format(RED, GREEN))
+            sleep(4)
     else:
         print("{0}Windows system not yet compatible. Make sure you're using a *Unix OS.{1}".format(RED, DEFAULT))
         exit()
